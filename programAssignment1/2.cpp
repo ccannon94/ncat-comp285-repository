161adf77e8baf869697e9c8f24afe52fe6e462e3
Chris Cannon <chris.c.canon@gmail.com>
2017-09-22 22:03:51 -0400

    implement array-based approach to josephus

--- programAssignment1/2.cpp
+++ programAssignment1/2.cpp
@@ -3,9 +3,13 @@
 
 using namespace std;
 
+int josephi(int n, int m, int p, int *players);
+int winningPlayer(int *players, int n);
+int activePlayer, turnNumber;
+
 int main(int argc, char *argv[])
 {
-  if(!argc == 4)
+  if(!(argc == 4))
   {
     std::cout<<"Incorrect parameters given for Josephus problem"<<std::endl;
     exit(0);
@@ -14,4 +18,37 @@ int main(int argc, char *argv[])
   int n = std::atoi(argv[1]);
   int m = std::atoi(argv[2]);
   int p = std::atoi(argv[3]);
+
+  int players[n];
+
+  for(int i = 0; i < n; i ++)
+  {
+    players[i] = i+1;
+  }
+
+  activePlayer = 0;
+  turnNumber = 1;
+
+  int winner = josephi(n, m, p, players);
+}
+
+int josephi(int n, int m, int p, int *players)
+{
+
+  if(n == 1)
+    return winningPlayer(players, n);
+
+
+}
+
+int winningPlayer(int *players, int n)
+{
+  int winner = 0;
+
+  for(int i = 0; i < n; i++)
+  {
+    winner = winner + players[i];
+  }
+
+  return winner;
 }
