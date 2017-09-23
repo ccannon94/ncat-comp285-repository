fb290b00913fb5abb3289e5db63abc28c543dcce
Chris Cannon <chris.c.canon@gmail.com>
2017-09-22 21:45:10 -0400

    Add input variables

--- programAssignment1/2.cpp
+++ programAssignment1/2.cpp
@@ -5,5 +5,13 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  
+  if(!argc == 4)
+  {
+    std::cout<<"Incorrect parameters given for Josephus problem"<<std::endl;
+    exit(0);
+  }
+
+  int n = std::atoi(argv[1]);
+  int m = std::atoi(argv[2]);
+  int p = std::atoi(argv[3]);
 }
