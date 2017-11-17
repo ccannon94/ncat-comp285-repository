57d4b8cc778e8a2a32850cf325fc6cee5f70b583
Chris Cannon <chris.c.canon@gmail.com>
2017-11-17 15:58:00 -0500

    Create CMake project

---
+++ programAssignment2/cmake-build-debug/CMakeFiles/programAssignment2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programAssignment2.dir/main.cpp.o"
+  "programAssignment2.pdb"
+  "programAssignment2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/programAssignment2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ programAssignment2/cmake-build-debug/CMakeFiles/programAssignment2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for programAssignment2.
+# This may be replaced when dependencies are built.
