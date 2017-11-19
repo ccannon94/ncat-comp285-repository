935fe7387d8c5807671750ba3c5b08af49bf45e4
Chris Cannon <chris.c.canon@gmail.com>
2017-11-19 16:39:32 -0500

    6 actually finished?

--- programAssignment2/6.cpp
+++ programAssignment2/6.cpp
@@ -15,8 +15,17 @@ int D(int n){
     To be graded, your program must be in your pgm and named precisely as
     6.cpp.
 */
+#include <iostream>;
+using namespace std;
 
+int D(int n);
 int knownD[100];
+
+int main(int argc, char* argv[]){
+    if(argc > 0)
+        std::cout << D(atoi(argv[0])) << std::endl;
+    return 0;
+}
 
 int D(int n) {
     if(n < 1) return -1;
