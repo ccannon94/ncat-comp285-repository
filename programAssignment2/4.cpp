f75ab64fbe9b65ebc069bb15d4d25281a0e10edf
Chris Cannon <chris.c.canon@gmail.com>
2017-11-19 17:10:37 -0500

    Complete 4

--- programAssignment2/4.cpp
+++ programAssignment2/4.cpp
@@ -10,22 +10,35 @@ itos function. (Use of other C functions is prohibitted)
 
 // this program will read in an integer and convert it to a string
 
+
+
+To be graded, your program must be in your pgm
+and named precisely as 4.cpp.
+ */
+
 #include <iostream>
 #include <cstdlib>
 #include <string>
 
 using namespace std;
 
+string returnString = "";
+
 string itos(int n) {
+    while(n)
+    {
+        int x=n%10;
+        n/=10;
+        char i='0';
+        i=i+x;
+        returnString=i+returnString;
+    }
+    return returnString;
 }
 
 
 int main(int argc, char* argv[]){
-	int n = atoi(argv[1]);
-	cout << "file "+ itos(n)+ " .txt" << endl;
-	return 0;
+    int n = atoi(argv[1]);
+    cout << "file "+ itos(n)+ " .txt" << endl;
+    return 0;
 }
-
-To be graded, your program must be in your pgm
-and named precisely as 4.cpp.
- */
